304311349b351b5f776dec433600c6c62ca6ad1d
Krishnan Rajiyah <krajiyah@gmail.com>
2018-04-17 19:01:27 -0700

    [feat] create user

--- models/User.js
+++ models/User.js
@@ -3,12 +3,25 @@ const firebase = require("../util/firebase.js");
 const overrideUtil = require("../util/overrides.js");
 const User = firebase.db.User;
 const fcm = firebase.fcm;
+const storage = firebase.storage;
 
 // PROTOTYPES
 
 
 // STATICS
 User.exists = async key => await User.getKeysExist([key]);
+User.notExists = async key => !(await User.exists(key));
+
+User.create = async params => {
+  let imageUrl = await storage.upload(params.image);
+  return await User.createByManualKey(params.key, {
+    firstName: params.firstName,
+    lastName: params.lastName,
+    phoneNumber: params.phoneNumber,
+    email: params.email,
+    imageUrl: imageUrl
+  });
+}
 
 // EXPORTS
 module.exports = User;
--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,12 @@
 const router = require("express").Router();
 const routerUtil = require("../util/router.js");
 const User = require("../models/User.js");
+const multer = require("multer");
+
+// CONSTANTS
+const upload = multer({
+  dest: '/tmp/'
+});
 
 // ROUTES
 router.get("/users/:key?", (req, res) => {
@@ -11,6 +17,19 @@ router.get("/users/:key?", (req, res) => {
   routerUtil.completeRequest(req, res, async params => {
     return await User.getByKey(params.key);
   });
+});
+
+router.post("/users", upload.single('image'), (req, res) => {
+  req.body.image = req.file;
+  req.checkBody("key", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("key", "user already exists").isAsyncFnTrue(User.notExists);
+  req.checkBody("image", router.errors.missingErrorMessage).notEmpty();
+  req.checkBody("image", router.errors.formatErrorMessage).isValidFile();
+  req.checkBody("firstName", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("lastName", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("phoneNumber", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("email", routerUtil.errors.missingErrorMessage).notEmpty();
+  routerUtil.completeRequest(req, res, User.create);
 });
 
 // EXPORTS
