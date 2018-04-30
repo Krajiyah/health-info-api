9ac01069849f555d2be459897f22af89f1b2d030
Krishnan Rajiyah <krajiyah@gmail.com>
2018-04-30 15:33:17 -0700

    [fix] remove unused dep

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const validator = require('express-validator-custom');
 const router = require("./routers/main.js");
 
