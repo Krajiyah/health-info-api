5dadd44b136285887b84b87bd7b51e5133ecb9c3
Krishnan Rajiyah <krajiyah@gmail.com>
2018-05-01 02:01:22 -0700

    [fix] use gmail transport

--- util/mail.js
+++ util/mail.js
@@ -4,23 +4,21 @@ const sanitize = require("sanitize-filename");
 
 // CONSTANTS
 const senderEmail = proccess.env.EMAIL;
-
-// SETUP
-nodemailer.SMTP = {
-  host: proccess.env.EMAIL_HOST,
-  port: parseInt(proccess.env.EMAIL_PORT),
-  use_authentication: true,
-  user: senderEmail,
-  pass: proccess.env.EMAIL_PASSWORD
-};
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: senderEmail,
+    pass: proccess.env.EMAIL_PASSWORD
+  }
+});
 
 // METHODS
 var sendWithAttachment = async(email, subjectText, text, filename, file) => {
   filename = sanitize(filename);
   let data = fs.readFileSync(file);
   return await new Promise(function(resolve reject) {
-      nodemailer.send_mail({
-          sender: senderEmail,
+      transporter.sendMail({
+          from: senderEmail,
           to: email,
           subject: subjectText,
           body: text,
