18cd1907e22c6dc19f34b6436290b6f3eda8e698
Krishnan Rajiyah <krajiyah@gmail.com>
2018-04-30 23:48:51 -0700

    [feat] POST appointment

--- routers/appointments.js
+++ routers/appointments.js
@@ -2,6 +2,7 @@
 const router = require("express").Router();
 const routerUtil = require("../util/router.js");
 const Appointment = require("../models/Appointment.js");
+const User = require("../models/User.js");
 
 router.get("/appointments/:key?", (req, res) => {
   req.checkQuery("key", routerUtil.errors.missingErrorMessage).notEmpty();
@@ -9,6 +10,22 @@ router.get("/appointments/:key?", (req, res) => {
     .isAsyncFnTrue(Appointment.exists);
   routerUtil.completeRequest(req, res, async params => {
     return await Appointment.getByKey(params.key);
+  });
+});
+
+router.post("/appointments", (req, res) => {
+  req.checkBody("user", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("user", routerUtil.errors.dbErrorMessage)
+    .isAsyncFnTrue(User.exists);
+  req.checkBody("name", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("time", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("time", routerUtil.errors.missingErrorMessage).isValidInteger();
+  routerUtil.completeRequest(req, res, async params => {
+    return await Appointment.createByAutoKey({
+      user: params.user,
+      name: params.name,
+      time: params.time
+    });
   });
 });
 
