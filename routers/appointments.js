78043b5c6dfd54cbcb7adb4de39710329933fc2d
Krishnan Rajiyah <krajiyah@gmail.com>
2018-05-01 01:33:16 -0700

    [feat] PATCH appointments (sign in)

--- routers/appointments.js
+++ routers/appointments.js
@@ -17,15 +17,23 @@ router.post("/appointments", (req, res) => {
   req.checkBody("user", routerUtil.errors.missingErrorMessage).notEmpty();
   req.checkBody("user", routerUtil.errors.dbErrorMessage)
     .isAsyncFnTrue(User.exists);
+  req.checkBody("institution", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkBody("institution", routerUtil.errors.dbErrorMessage)
+    .isAsyncFnTrue(Institution.exists);
   req.checkBody("name", routerUtil.errors.missingErrorMessage).notEmpty();
   req.checkBody("time", routerUtil.errors.missingErrorMessage).notEmpty();
   req.checkBody("time", routerUtil.errors.missingErrorMessage).isValidInteger();
+  routerUtil.completeRequest(req, res, Appointment.create);
+});
+
+router.patch("/appointments/:key", (req, res) => {
+  req.checkParams("key", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkParams("key", routerUtil.errors.dbErrorMessage)
+    .isAsyncFnTrue(Appointment.exists);
   routerUtil.completeRequest(req, res, async params => {
-    return await Appointment.createByAutoKey({
-      user: params.user,
-      name: params.name,
-      time: params.time
-    });
+    let a = await Appointment.getByKey(params.key);
+    await a.signIn();
+    return a;
   });
 });
 
