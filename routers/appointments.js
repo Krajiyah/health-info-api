5603b0cf18adff92e273f6a029a1cb7cf1b0a461
Krishnan Rajiyah <krajiyah@gmail.com>
2018-04-30 23:39:21 -0700

    [feat] GET appointment

--- routers/appointments.js
+++ routers/appointments.js
@@ -3,5 +3,14 @@ const router = require("express").Router();
 const routerUtil = require("../util/router.js");
 const Appointment = require("../models/Appointment.js");
 
+router.get("/appointments/:key?", (req, res) => {
+  req.checkQuery("key", routerUtil.errors.missingErrorMessage).notEmpty();
+  req.checkQuery("key", routerUtil.errors.dbErrorMessage)
+    .isAsyncFnTrue(Appointment.exists);
+  routerUtil.completeRequest(req, res, async params => {
+    return await Appointment.getByKey(params.key);
+  });
+});
+
 // EXPORTS
 module.exports = router;
